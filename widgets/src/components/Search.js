211594eea70f811e2de5b76184db8cda4731c135
youngho <im0505>
2020-07-30 00:49:43 +0900

    temp

---
+++ widgets/src/components/Search.js
@@ -0,0 +1,22 @@
+import React, { useState } from "react";
+
+const Search = () => {
+  const [term, setTerm] = useState("");
+
+  return (
+    <div>
+      <div className="ui form">
+        <div className="field">
+          <label>Enter Search Term</label>
+          <input
+            className="input"
+            onChange={(e) => {
+              setTerm(e.target.value);
+            }}
+          ></input>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Search;
