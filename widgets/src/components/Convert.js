3c0992bc6b426687a54bb712beb40711c3c4b17e
youngho <im0505>
2020-07-30 23:40:03 +0900

    debouncing technique

--- widgets/src/components/Convert.js
+++ widgets/src/components/Convert.js
@@ -3,14 +3,23 @@ import axios from 'axios';
 
 const Convert = ({ language, text }) => {
     const [translated, setTranslated] = useState('')
+    const [debouncedText, setDebouncedText] = useState(text);
 
+    useEffect(() => {
+        const timerId = setTimeout(() => {
+            setDebouncedText(text)
+        }, 550)
+        return () => {
+            clearTimeout(timerId)
+        }
+    }, [text])
     useEffect(() => {
         const translate = async () => {
             const { data } = await axios.post(`https://translation.googleapis.com/language/translate/v2`,
                 {},
                 {
                     params: {
-                        q: text,
+                        q: debouncedText,
                         target: language.value,
                         key: "AIzaSyCHUCmpR7cT_yDFHC98CZJy2LTms-IwDlM"
 
@@ -20,7 +29,7 @@ const Convert = ({ language, text }) => {
             setTranslated(data.data.translations[0].translatedText)
         }
         translate()
-    }, [language, text])
+    }, [debouncedText])
 
 
     return (<div>
